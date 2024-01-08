94ef4ae9fa39c5d7d72b03eecf14f0a829645d7e
chrinovic mm <chrinovicmukeba123@gmail.com>
2024-01-08 14:28:02 +0200

    repprise du projet

--- Yveschallenge/app/src/main/java/com/chrinovicmm/yves_challenge/MainActivity.kt
+++ Yveschallenge/app/src/main/java/com/chrinovicmm/yves_challenge/MainActivity.kt
@@ -20,15 +20,30 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.chrinovicmm.yves_challenge.ui.theme.YveschallengeTheme
@@ -61,7 +76,7 @@ fun StatusComponent(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            painter = painterResource(R.drawable.ic_launcher_background),
+            painter = painterResource(R.drawable.profile),
             contentDescription = null,
             modifier = Modifier
                 .size(50.dp)
@@ -105,9 +120,9 @@ fun StoriesComponent(
         Column(
             modifier = Modifier.width(100.dp),
             verticalArrangement = Arrangement.Center
-        ) {
+        ){
             Image(
-                painter = painterResource(R.drawable.ic_launcher_background),
+                painter = painterResource(R.drawable.chris),
                 contentDescription = null,
                 modifier = Modifier.size(100.dp),
                 contentScale = ContentScale.Crop
@@ -121,6 +136,82 @@ fun StoriesComponent(
                     .padding(start = 10.dp)
             )
         }
+    }
+}
+
+@Composable
+fun SootheBottomNavigation(modifier: Modifier = Modifier) {
+    NavigationBar(
+        modifier = modifier,
+        containerColor = MaterialTheme.colorScheme.surfaceVariant
+    ) {
+        NavigationBarItem(
+            selected = false,
+            onClick = {},
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = "Home"
+                )
+            }
+        )
+
+        NavigationBarItem(
+            selected = false,
+            onClick = {},
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.ShoppingCart,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = "Cart"
+                )
+            }
+        )
+
+        NavigationBarItem(
+            selected = false,
+            onClick = {},
+            icon = {
+                Image(
+                    painter = painterResource(id = R.drawable.profile),
+                    contentDescription = null,
+                    modifier = Modifier.size(50.dp)
+                )
+            },
+            label = {
+                Text(
+                    text = "Cart"
+                )
+            }
+        )
+
+
+
+
+        NavigationBarItem(
+            selected = false,
+            onClick = {},
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = "Profile"
+                )
+            }
+        )
     }
 }
 
@@ -142,6 +233,34 @@ fun StoriesComponents(
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MySootheAppPortrait() {
+    YveschallengeTheme {
+        Scaffold(
+            bottomBar = { SootheBottomNavigation() }
+        ){padding->
+            HomeScreen(Modifier.padding(padding))
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun topBar(){
+    TopAppBar(
+        title = {},
+        navigationIcon = {
+            IconButton(onClick = {  }) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = null
+                )
+            }
+        }
+    )
+}
 
 
 @Composable
@@ -169,19 +288,22 @@ fun HomeScreen(
     Column(
         modifier.verticalScroll(rememberScrollState())
     ) {
-        Spacer(modifier.height(16.dp))
+        topBar()
+        //Spacer(modifier.height(16.dp))
         HomeSection(title = "Friends online"){
             StatusComponents()
         }
         HomeSection(title = "Friends Stories"){
             StoriesComponents()
         }
+
+
     }
 }
 
 @Composable
 fun App(){
-    HomeScreen()
+    MySootheAppPortrait()
 }
 
 @Preview(showBackground = true)
