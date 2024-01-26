242a7c80f4f6c149b7e3b58fca678f7b85f36bed
chrinovic mm <chrinovicmukeba123@gmail.com>
2024-01-26 08:13:36 +0200

    finish Screens

--- instagramClone/app/src/main/java/com/chrinovicmm/instagramclone/MainActivity.kt
+++ instagramClone/app/src/main/java/com/chrinovicmm/instagramclone/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.chrinovicmm.instagramclone.composables.Screen
 import com.chrinovicmm.instagramclone.ui.theme.InstagramCloneTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +23,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Screen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    InstagramCloneTheme {
-        Greeting("Android")
-    }
-}
