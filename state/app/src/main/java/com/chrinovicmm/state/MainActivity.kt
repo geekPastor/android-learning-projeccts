01f901148a1c4caa2d6411fdca9271ff20cfcc90
chrinovic mm <chrinovicmukeba123@gmail.com>
2024-02-06 21:31:22 +0200

    add lits project

--- state/app/src/main/java/com/chrinovicmm/state/MainActivity.kt
+++ state/app/src/main/java/com/chrinovicmm/state/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.chrinovicmm.state
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.chrinovicmm.state.ui.theme.StateTheme
@@ -35,13 +38,22 @@ class MainActivity : ComponentActivity() {
 @SuppressLint("UnrememberedMutableState")
 @Composable
 fun MyScreen(){
+    var nombre by rememberSaveable {
+        mutableStateOf(0)
+    }
     Column {
-        var nombre by remember {
-            mutableStateOf(0)
-        }
-        Button(onClick = { nombre++ }) {
-            Text(text = "Clique ${nombre} fois !")
-        }
+        Text(text = "Clique ${nombre} fois !")
+        IncrementeButton(nombre, {nombre++})
+    }
+}
+
+@Composable
+fun IncrementeButton(nombre: Int, onIncrement: ()->Unit){
+    Button(
+        onClick = onIncrement,
+        colors = if (nombre > 2) ButtonDefaults.buttonColors(androidx.compose.ui.graphics.Color.Green) else ButtonDefaults.buttonColors()
+    ) {
+        Text(text = "Clique !")
     }
 }
 
