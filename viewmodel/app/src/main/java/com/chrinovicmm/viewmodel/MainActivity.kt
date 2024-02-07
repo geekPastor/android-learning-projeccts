cb1a976229a279cf9ad533c269f36ee70e23ecd1
chrinovic mm <chrinovicmukeba123@gmail.com>
2024-02-08 00:47:14 +0200

    finish state and start viewmodel

--- viewmodel/app/src/main/java/com/chrinovicmm/viewmodel/MainActivity.kt
+++ viewmodel/app/src/main/java/com/chrinovicmm/viewmodel/MainActivity.kt
@@ -1,53 +1,46 @@
-package com.chrinovicmm.listes
+package com.chrinovicmm.viewmodel
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.chrinovicmm.listes.ui.theme.ListesTheme
+import com.chrinovicmm.viewmodel.ui.theme.ViewmodelTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ListesTheme {
+            ViewmodelTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MyScreen()
+                    Greeting("Android")
                 }
             }
         }
     }
 }
 
-
 @Composable
-fun MyScreen(){
-    LazyColumn{
-        items(Data.listPersons){fullName->
-            PersonItem(fullname = fullName)
-        }
-    }
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ListesTheme {
-        MyScreen()
+    ViewmodelTheme {
+        Greeting("Android")
     }
 }
